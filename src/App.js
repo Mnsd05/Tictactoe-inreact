dd1fc4de8b276732182538b9affc0acd5302eed8
Nguyen Duc Thang <thangngdc03bn@gmail.com>
2023-02-13 20:55:03 +0800

    change BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import {HashRouter as Router, Routes, Route} from "react-router-dom"
 import Choose from "./Choose";
 import Tictactoe from "./Tictactoe";
 
