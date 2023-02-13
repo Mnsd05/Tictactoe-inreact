a9d6f4ad26730721d358be3ecd2d094682ecc5d1
Nguyen Duc Thang <thangngdc03bn@gmail.com>
2023-02-13 17:23:58 +0800

    Extract files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react"
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Choose from "./Choose";
 import Tictactoe from "./Tictactoe";
-// function check: return "f" if the game is not done yet, "w" if there is a winner and "d" if there is no winners
 
 export default function App() {
     return (
